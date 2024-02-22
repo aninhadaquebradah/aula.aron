8d40ffc83c096fc4494c5667338f7f2e86dfd271
aninhadaquebradah <132685023+aninhadaquebradah@users.noreply.github.com>
2024-02-22 08:55:01 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "832.192.304-34"
+
+console.log(`conteudo inicial da variavel: ${pessoa} | Tipo: ${typeof(pessoa)}`);
+
+pessoa = 259;
+
+console.log(`mudei o conteudo: ${pessoa} | Tipo: ${typeof(pessoa)}`); 
