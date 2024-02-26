f5d4564658e9403856657c450da97677d6d0ceeb
aninhadaquebradah <132685023+aninhadaquebradah@users.noreply.github.com>
2024-02-26 11:42:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log(`conteudo inicial da variavel: ${pessoa} | Tipo: ${typeof(pessoa)}`)
 pessoa = 259;
 
 console.log(`mudei o conteudo: ${pessoa} | Tipo: ${typeof(pessoa)}`); 
+
+var num1 = 10
+var num2 = 20
+
+var total = 0
+
+console.log(`Total é: ${total}`);
+
+total = num1 + num2;
+
+console.log(`Total da soma ${num1} +${num2}=${total}`);
+
+total = num1 - num2;
+
+console.log(`total da subtração ${num1}-${num2}=${total}`);
